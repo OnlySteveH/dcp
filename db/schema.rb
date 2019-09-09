19aacc2a55f46626bb0461bbdb0a5a624440051d
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-09-09 16:56:57 +0100

    added a scaffold for Blog

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2019_09_09_154949) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
