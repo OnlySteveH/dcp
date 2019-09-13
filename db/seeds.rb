f76b733bacd8e88d3ead8065832c21ffd0298a0c
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-09-13 11:11:01 +0100

    integrate index action for Portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ end
 
 9.times do |portfolio_item|
 	Portfolio.create!(
-		title: "Portfolio tile #{portfolio_item}",
+		title: "Portfolio title #{portfolio_item}",
 		subtitle: "My great service",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
 		main_image: "https://placehold.it/600x400",
