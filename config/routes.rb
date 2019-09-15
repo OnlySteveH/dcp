20bba060bc4273a3977eab8f2407e571c52d0d7c
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-09-15 11:45:19 +0100

    adding custom routes to Portfolio resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 	
-  resources :portfolios
+  resources :portfolios, except: [:show]
   resources :blogs
 
   root to: 'pages#home'
 
   get 'about', 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
 end
