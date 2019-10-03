c51c743c50df5df00024cfe00811e8db7dce770c
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-10-03 18:04:39 +0100

    adding scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+3.times do |topic|
+	Topic.create(title: "Topic #{topic}")
+	puts "Topic #{topic} created"
+end
+
 10.times do |blog|
  	Blog.create!(
  		title: "My blog post #{blog}",
- 		body: "A body for blog #{blog}. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+ 		body: "A body for blog #{blog}. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		topic_id: Topic.last.id 	
  	)
  	puts "Blog post #{blog} created"
 end
@@ -14,10 +20,21 @@ end
 	puts "Skill #{skill} created"
 end
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title #{portfolio_item}",
-		subtitle: "My great service",
+		subtitle: "Ruby on Rails",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		main_image: "https://placehold.it/600x400",
+		thumb_image: "https://placehold.it/350x200"
+	)
+	puts "Portfolio #{portfolio_item} created"
+end
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio new title #{portfolio_item}",
+		subtitle: "Software engineering",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
 		main_image: "https://placehold.it/600x400",
 		thumb_image: "https://placehold.it/350x200"
