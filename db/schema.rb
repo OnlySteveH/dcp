dc87749e57c87bcd9ab1dd1807355f96aa67842b
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-10-10 16:30:48 +0100

    added concern to manage image URL

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_162451) do
+ActiveRecord::Schema.define(version: 2019_10_10_151516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_10_02_162451) do
     t.integer "percent_utilised"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
