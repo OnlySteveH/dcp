f3acde314890c4e176dfb9db0bd8dc1c68eaa457
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-11-01 23:36:56 +0000

    installed jquery and sortable html5 plus css calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     else
       (link_to 'Log in', user_session_path, class: style) + 
-      " ".html_safe +
+      "  ".html_safe + 
       (link_to 'Register', new_user_registration_path, class: style)
     end
 	end
